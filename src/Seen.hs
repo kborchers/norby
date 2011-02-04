2842c8fa55fb10fef844c8f3fed334885f8f70f9
nlogax <jonas.westerlund+github@gmail.com>
2011-02-04 22:36:57 +0100

    Made the seen thing reply to queries

--- src/Seen.hs
+++ src/Seen.hs
@@ -33,17 +33,17 @@ store _ = return ()
 
 seen :: Message -> Net ()
 seen (Message (Just (NickName n _ _)) _ params)
-    | ln    == lnick  = privmsg chan $ printf "%s: That's you, right there." n
-    | lnick == S.nick = privmsg chan $ printf "%s: That's me." n
+    | ln    == lnick  = privmsg target $ printf "%s: That's you, right there." n
+    | lnick == S.nick = privmsg target $ printf "%s: That's me." n
     | otherwise       = do
         cp <- asks pool
         qr <- runDb cp $ findNick lnick
         case qr of
-                (Left _)  -> privmsg chan "Connection error."
+                (Left _)  -> privmsg target "Connection error."
                 (Right v) -> result v
     where findNick n  = findOne (select [ "nick" =: n ] collection) { sort = [ "_id" =: (-1 :: Int) ]}
           timeAgo d m = U.relTime . round $ diffUTCTime d m
-          result Nothing  = privmsg chan $ printf
+          result Nothing  = privmsg target $ printf
                             "%s: I have never seen %s." n nick
           result (Just d) = do
               now <- liftIO getCurrentTime
@@ -51,13 +51,16 @@ seen (Message (Just (NickName n _ _)) _ params)
               let cmd = B.at "what" d
               let chn = B.at "chan" d
               let whn = B.at "date" d
-              privmsg chan $ printf "%s: %s was seen %s, %s"
-                                    n nick (timeAgo now whn)
-                                    (formatSeen txt cmd chn)
+              privmsg target $ printf "%s: %s was seen %s, %s"
+                                      n nick (timeAgo now whn)
+                                      (formatSeen txt cmd chn)
           ln    = map toLower n
           lnick = map toLower nick
           nick  = U.trim . concat . take 1 . drop 1 . words . last $ params
           chan  = concat $ take 1 params
+          target | chan == S.nick = n
+                 | otherwise      = chan
+
 seen _ = return ()
 
 formatSeen :: String -> String -> String -> String
